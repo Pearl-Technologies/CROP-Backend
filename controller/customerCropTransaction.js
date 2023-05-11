f6b71175e803425b424f7d7d33b2130c5ab7c295
Pearl-Technologies <itteam@gmail.com>
2023-05-11 10:26:52 +0530

    Changes

--- controller/customerCropTransaction.js
+++ controller/customerCropTransaction.js
@@ -1,10 +1,19 @@
 const customerCropTransaction = require("../models/CropTransaction");
 const mongoose = require("mongoose");
 const ObjectId = mongoose.Types.ObjectId;
+const { User } = require("../models/User");
+const {Token} = require("../models/User");
+const pdfkit = require('pdfkit');
+
+const fs = require('fs');
+const pdfPath = process.cwd() + "/uploads/";
+const nodemailer = require('nodemailer');
 
 const getMyCropTrasaction = async (req, res) => {
-  const { user, startDate, endDate, search } = req.query;
-
+  const { startDate, endDate, search } = req.query;
+  let token= req.headers.authorization
+  const token_data = await Token.findOne({ token });
+  let user= token_data.user;
   try {
     let findone = await customerCropTransaction.find({
       user: mongoose.Types.ObjectId(`${user}`),
@@ -25,7 +34,7 @@ const getMyCropTrasaction = async (req, res) => {
           $match: {
             createdAt: {
               $gte: new Date(startDate),
-              $lte: new Date(startDate),
+              $lte: new Date(endDate),
             },
           },
         },
@@ -139,6 +148,173 @@ const getMyCropTrasaction = async (req, res) => {
     res.status(500).send({ msg: "internal server error", status: 500 });
   }
 };
+
+const getEmailStatementMyCropTrasaction = async (req, res) => {
+  const { startDate, endDate, email } = req.query;
+  let token = req.headers.authorization;
+  const token_data = await Token.findOne({ token });
+  let user = token_data.user;
+  let adddata = await User.find({_id:user})
+  try {
+    let findone = await customerCropTransaction.find({
+      user: mongoose.Types.ObjectId(`${user}`),
+    });
+    if (!findone.length) {
+      return res
+        .status(200)
+        .send({ msg: "no order", data: findone, status: 200 });
+    }
+    if (startDate && endDate) {
+      const trasactionDetails = await customerCropTransaction.aggregate([
+        {
+          $match: {
+            user: { $eq: findone[0].user },
+          },
+        },
+        {
+          $match: {
+            createdAt: {
+              $gte: new Date(startDate),
+              $lte: new Date(endDate),
+            },
+          },
+        },
+        {
+          $lookup: {
+            from: "customer_payment_trackers",
+            localField: "orderNumber",
+            foreignField: "payment_intent",
+            as: "pt",
+          },
+        },
+        {
+          $unwind: "$pt",
+        },
+        {
+          $project: {
+            orderNumber: 1,
+            transactionType: 1,
+            crop: 1,
+            amount: 1,
+            description: 1,
+            pt: {
+              invoice_url: 1,
+              invoice_pdf: 1,
+            },
+            createdAt: 1,
+          },
+        },
+        { $sort: { createdAt: -1 } },
+      ]);
+
+      // Create a new PDF document
+      // const pdfDoc = new pdfkit();
+      
+      // Set the filename for the PDF
+      // const filename = `MyCropTransaction-${new Date().toISOString()}.pdf`;
+      const filename = `MyCropTransaction.pdf`;
+      const writeStream = fs.createWriteStream('MyCropTransaction.pdf');
+      // pdfDoc.pipe(writeStream);
+
+      // // Add content to the PDF document
+      // pdfDoc.text('My PDF Document');
+      // trasactionDetails.forEach(transaction => {
+      //   pdfDoc.fontSize(14).text(`Order Number: ${transaction.orderNumber}`);
+      //   pdfDoc.fontSize(12).text(`Transaction Type: ${transaction.transactionType}`);
+      //   pdfDoc.fontSize(12).text(`Crop: ${transaction.crop}`);
+      //   pdfDoc.fontSize(12).text(`Amount: ${transaction.amount}`);
+      //   pdfDoc.fontSize(12).text(`Description: ${transaction.description}`);
+      //   pdfDoc.fontSize(12).text(`Created At: ${transaction.createdAt}`);
+      //   });
+      // pdfDoc.end();
+
+      const headers = ['Order Number', 'Transaction Type', 'Crop', 'Amount', 'Description', 'Created At'];
+const rows = trasactionDetails.map(transaction => [
+  transaction.orderNumber,
+  transaction.transactionType,
+  transaction.crop,
+  transaction.amount,
+  transaction.description,
+  transaction.createdAt,
+]);
+
+const docDefinition = {
+  content: [
+    { text: 'My CROP Transaction', style: 'header' },
+    {
+      table: {
+        headers: headers,
+        body: rows
+      }
+    }
+  ],
+  styles: {
+    header: {
+      fontSize: 18,
+      bold: true,
+      margin: [0, 0, 0, 10]
+    }
+  }
+};
+
+const pdfDoc = new pdfkit();
+pdfDoc.pipe(writeStream).on('finish', () => {
+  console.log('PDF saved');
+});
+pdfDoc.text(JSON.stringify(docDefinition));
+pdfDoc.end();
+
+      // writeStream.on('finish', () => {
+      //   console.log('PDF file created successfully');
+      // });
+      const transporter = nodemailer.createTransport({
+        host: process.env.HOST,
+        service: process.env.SERVICE, //comment this line if you use custom server/domain
+        port: process.env.EMAIL_PORT,
+        secure: true,
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASS,
+        },
+      });
+
+      // Define the email options
+      const mailOptions = {
+        from: process.env.EMAIL_USER,
+        to: adddata[0].email,
+        subject: 'My Crop Transactions',
+        text: 'Please find attached the PDF of your crop transactions',
+        attachments: [
+          {
+            filename,
+            content: fs.createReadStream(filename),
+          },
+        ],
+      };
+
+      // Send the email with the PDF attachment
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          console.log(error);
+          return res.status(500).send({ error });
+        } else {
+          console.log(`Email sent: ${info.response}`);
+          return res.status(200).send({ status: 200, message: "Successfully Sent." });
+        }
+      });
+
+      // Delete the PDF file after sending the email
+      // fs.unlinkSync(filename);
+    }
+    else{
+      return res.status(500).send({message: "Please sent start date and end date" });  
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({ error });
+  }
+}
+
 const SaveMyCropTrasaction = async (
   amount,
   crop,
@@ -165,4 +341,4 @@ const SaveMyCropTrasaction = async (
   }
 };
 
-module.exports = { getMyCropTrasaction, SaveMyCropTrasaction };
+module.exports = { getMyCropTrasaction, SaveMyCropTrasaction, getEmailStatementMyCropTrasaction };
--- routes/customerCropTrasaction.js
+++ routes/customerCropTrasaction.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const {getMyCropTrasaction} = require("../controller/customerCropTransaction");
+const {getMyCropTrasaction, getEmailStatementMyCropTrasaction} = require("../controller/customerCropTransaction");
 const {getMyPropTrasaction} = require("../controller/customerPropTransaction");
 
 router.get("/getMyCropTrasaction", getMyCropTrasaction)
 router.get("/getMyPropTrasaction", getMyPropTrasaction)
+router.get('/getEmailStatementMyCropTrasaction', getEmailStatementMyCropTrasaction);
 module.exports = router;
