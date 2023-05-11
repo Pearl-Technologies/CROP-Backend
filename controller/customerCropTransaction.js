577d082e7b547c97615c4c26cc8ef56c5f39f1da
Pearl-Technologies <itteam@gmail.com>
2023-05-11 10:31:49 +0530

    Admin changes

--- controller/customerCropTransaction.js
+++ controller/customerCropTransaction.js
@@ -315,6 +315,147 @@ pdfDoc.end();
   }
 }
 
+const getAllCropTrasactionByAdmin = async (req, res) => {
+
+  const { user, startDate, endDate, search } = req.query;
+  
+  try {
+    let findone = await customerCropTransaction.find({
+      user: mongoose.Types.ObjectId(`${user}`),
+    });
+    
+    if (!findone.length) {
+      return res
+        .status(200)
+        .send({ msg: "no order", data: findone, status: 200 });
+    }
+    if (startDate && endDate) {
+      const trasactionDetails = await customerCropTransaction.aggregate([
+        {
+          $match: {
+            user: { $eq: findone[0].user },
+          },
+        },
+        {
+          $match: {
+            createdAt: {
+              $gte: new Date(startDate),
+              $lte: new Date(startDate),
+            },
+          },
+        },
+        {
+          $lookup: {
+            from: "customer_payment_trackers",
+            localField: "orderNumber",
+            foreignField: "payment_intent",
+            as: "pt",
+          },
+        },
+        {
+          $unwind: "$pt",
+        },
+        {
+          $project: {
+            orderNumber: 1,
+            transactionType: 1,
+            crop: 1,
+            amount: 1,
+            description: 1,
+            pt: {
+              invoice_url: 1,
+              invoice_pdf: 1,
+            },
+            createdAt: 1,
+          },
+        },
+        { $sort: { createdAt: -1 } },
+      ]);
+      return res.status(200).send({ data: trasactionDetails, status: 200 });
+    }
+    if (search) {
+      const trasactionDetails = await customerCropTransaction.aggregate([
+        {
+          $match: {
+            user: { $eq: findone[0].user },
+          },
+        },
+        {
+          $lookup: {
+            from: "customer_payment_trackers",
+            localField: "orderNumber",
+            foreignField: "payment_intent",
+            as: "pt",
+          },
+        },
+        {
+          $unwind: "$pt",
+        },
+        {
+          $project: {
+            orderNumber: 1,
+            transactionType: 1,
+            crop: 1,
+            amount: 1,
+            description: 1,
+            pt: {
+              invoice_url: 1,
+              invoice_pdf: 1,
+            },
+            createdAt: 1,
+          },
+        },
+        {
+          $match: {
+            orderNumber: search,
+          },
+        },
+        { $sort: { createdAt: -1 } },
+      ]);
+      return res.status(200).send({ data: trasactionDetails, status: 200 });
+    }
+
+    const trasactionDetails = await customerCropTransaction.aggregate([
+      {
+        $match: {
+          user: { $eq: findone[0].user },
+        },
+      },
+      {
+        $lookup: {
+          from: "customer_payment_trackers",
+          localField: "orderNumber",
+          foreignField: "payment_intent",
+          as: "pt",
+        },
+      },
+      {
+        $unwind: "$pt",
+      },
+      {
+        $project: {
+          orderNumber: 1,
+          transactionType: 1,
+          crop: 1,
+          amount: 1,
+          description: 1,
+          pt: {
+            invoice_url: 1,
+            invoice_pdf: 1,
+          },
+          createdAt: 1,
+        },
+      },
+      { $sort: { createdAt: -1 } },
+    ]);
+    
+    return res.status(200).send({ trasactionDetails, status: 200 });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ msg: "internal server error", status: 500 });
+  }
+};
+
 const SaveMyCropTrasaction = async (
   amount,
   crop,
@@ -341,4 +482,4 @@ const SaveMyCropTrasaction = async (
   }
 };
 
-module.exports = { getMyCropTrasaction, SaveMyCropTrasaction, getEmailStatementMyCropTrasaction };
+module.exports = { getMyCropTrasaction, SaveMyCropTrasaction, getEmailStatementMyCropTrasaction, getAllCropTrasactionByAdmin };
