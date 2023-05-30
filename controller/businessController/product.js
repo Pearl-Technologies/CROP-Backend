f4809979f4ecae1f9ea2c3676621805d637f61c9
SridharIyappan <sridharsp4774@gmail.com>
2023-05-30 17:53:52 +0530

    Promo change

--- controller/businessController/product.js
+++ controller/businessController/product.js
@@ -2195,12 +2195,11 @@ module.exports.getPromoEarnAndRedeemProducts = async (req, res) => {
     const endIndex = startIndex + limit
     const earnCropProducts = await this.getEarnCropProducts(req, res)
     const redeemCropProducts = await this.getRedeemCropProducts(req, res)
-    // const promoProducts = await Product.find({})
-    if(redeemCropProducts.length != 0){
-      const promoProducts = earnCropProducts.concat(redeemCropProducts)  
-    }
-    else{
-      const promoProducts = earnCropProducts
+    let promoProducts = []
+    if (redeemCropProducts.length != 0) {
+      promoProducts = earnCropProducts.concat(redeemCropProducts)
+    } else {
+      promoProducts = earnCropProducts
     }
     const promoProductsWithLimit = promoProducts.slice(startIndex, endIndex)
     console.log({ startIndex, endIndex })
